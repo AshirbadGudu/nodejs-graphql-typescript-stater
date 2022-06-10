10e7f3c224c8e6189b9647f7848ed999c1fd0b62
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2022-06-10 23:05:52 +0530

    For parsing application/json type body middlewares added

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,10 @@ const PORT = process.env.PORT || 3023;
 
 const app = express();
 
+// For parsing application/json type body add following middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.get("/", (_, res) => res.send("Node JS Express Typescript Server Running"));
 
 /**
