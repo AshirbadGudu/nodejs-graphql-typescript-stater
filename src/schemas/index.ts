10ea265017bfc0893844dfd0d6ea2ce5790e4eda
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2023-01-01 21:29:48 +0530

    query and mutation schema moved to graphql folder

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,15 +1,8 @@
 import { buildSchema } from "graphql";
 import { readGraphql } from "../utils";
 
-const UserSchema = readGraphql({ schema: "user" });
-
 export const schema = buildSchema(`
-    ${UserSchema}
-    type Query {
-        users: [User]
-        user(_id: String!): User
-    }
-    type Mutation {
-        createUser(user: UserInput): User
-    }
+    ${readGraphql({ schema: "user" })}
+    ${readGraphql({ schema: "query" })}
+    ${readGraphql({ schema: "mutation" })}    
 `);
