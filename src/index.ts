07fb19eef674d7246b2fe752ef8fe77f5ef4d1c4
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2023-01-01 19:44:48 +0530

    express-graphql packages installed and basic init complete

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { config } from "dotenv";
 import express from "express";
 import { users } from "./routers";
+import { GraphQLSchema, GraphQLObjectType, GraphQLString } from "graphql";
+import { graphqlHTTP } from "express-graphql";
+
 config();
 
 const PORT = process.env.PORT || 3023;
@@ -18,5 +21,29 @@ app.get("/", (_, res) => res.send("Node JS Express Typescript Server Running"));
  * */
 
 app.use("/users/", users);
+
+/**
+ * Create a graphql
+ * */
+
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: "helloworld",
+    fields: {
+      message: {
+        type: GraphQLString,
+        resolve: () => "Hello World!",
+      },
+    },
+  }),
+});
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    graphiql: true,
+  })
+);
 
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
